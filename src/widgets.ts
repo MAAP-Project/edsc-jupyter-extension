cd99683b6acbeb290ba639246daadde56e2a42d8
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2023-09-13 15:45:18 -0700

    switching to proxy approach

--- src/widgets.ts
+++ src/widgets.ts
@@ -1,5 +1,5 @@
 import { Widget } from '@lumino/widgets';
-//import { PageConfig } from '@jupyterlab/coreutils'
+import { PageConfig } from '@jupyterlab/coreutils'
 import { Notification } from '@jupyterlab/apputils';
 
 import {
@@ -11,7 +11,7 @@ import globals from "./globals";
 
 let unique = 0;
 
-/*export
+export
 class IFrameWidget extends Widget {
 
   constructor(path: string) {
@@ -53,16 +53,70 @@ class IFrameWidget extends Widget {
     div.appendChild(iframe);
     this.node.appendChild(div);
   }
-};*/
+};
 
 
 //
 // Widget to display Earth Data Search Client inside an iframe
 //
+
+/*export class IFrameWidget extends Widget {
+  private iframe: HTMLIFrameElement;
+
+  constructor(initialPath: string) {
+    super();
+    this.id = initialPath + '-' + unique;
+    unique += 1;
+
+    this.title.label = "Earthdata Search";
+    this.title.closable = true;
+
+    let div = document.createElement('div');
+    div.classList.add('iframe-widget');
+    this.iframe = document.createElement('iframe');
+    this.iframe.id = "iframeid";
+
+    // Set the initial URL
+    this.iframe.src = initialPath;
+
+    // Add an event listener for the iframe's load event
+    this.iframe.addEventListener('load', () => {
+      // Check if the URL has changed due to redirection
+      console.log("graceal1 in iframe add event listener ");
+      console.log(this.iframe.src);
+      if (this.iframe.src !== initialPath) {
+        console.log('Redirected to:', this.iframe.src);
+      }
+    });
+
+    div.appendChild(this.iframe);
+    this.node.appendChild(div);
+
+    // Add a message event listener to handle cross-origin communication
+    window.addEventListener('message', (event) => this.handleMessage(event));
+  }
+
+  private handleMessage(event: MessageEvent): void {
+    console.log("graceal1 in handle mesage function");
+    console.log(event);
+    // Ensure the message is from the iframe
+    if (event.source === this.iframe.contentWindow) {
+      // Handle the message data as needed
+      console.log('Received message:', event.data);
+    }
+  }
+
+  // Method to navigate the iframe to a new URL
+  public navigateToUrl(url: string): void {
+    // Update the iframe's source to the specified URL
+    this.iframe.src = url;
+  }
+}*/
+/*
 export class IFrameWidget extends Widget {
   private iframe: HTMLIFrameElement;
   //private originalPath: string;
-  private currentPath: string;
+  //private currentPath: string;
 
   constructor(path: string) {
     super();
@@ -79,32 +133,87 @@ export class IFrameWidget extends Widget {
 
     // Store the original path
     //this.originalPath = path;
-    this.currentPath = path;
+    //this.currentPath = path;
 
     // Add the iframe to the widget
     div.appendChild(this.iframe);
     this.node.appendChild(div);
 
     // Set up an event listener to handle URL changes in the iframe
-    this.iframe.addEventListener('load', this.handleIFrameLoad.bind(this));
+    this.iframe.addEventListener('load', this.handleIFrameLoad.bind(this));*/
+    /*this.iframe.addEventListener('load', (event) => { // initially was "click", but "load" is actually catching
+      // I am wondering if load negates the need to check event.target.tagName... more testing needed 
+      const iframeDocument = this.iframe.contentDocument;
+      console.log("graceal1 iframe document");
+      console.log(iframeDocument);
+
+      if (iframeDocument) {
+        // Access the iframe's document
+        const anchorElements = iframeDocument.querySelectorAll('a');
+        console.log(anchorElements);
+
+        anchorElements.forEach((anchor) => {
+          anchor.addEventListener('click', (event) => {
+            console.log("graceal1 in anchor element click event with ");
+            console.log(event);
+            event.preventDefault(); // Prevent the default link behavior
+            const link = anchor.href; // Get the link URL
+            // Open the link in a new tab or window
+            window.open(link, '_blank');
+          });
+        });
+      }*/
+      /*console.log("graceal1 in add event listener for iframe and event is ");
+      console.log(event);
+      console.log(event.target);
+      console.log(event.target instanceof HTMLElement);
+      if (event.target && event.target instanceof HTMLElement) {
+        console.log('graceal1 in first if statement printing tagName');
+        console.log(event.target.tagName);
+      }
+      if (event.target && event.target instanceof HTMLElement) {
+        console.log("graceal1 entered if statement seeing that link was pressed ");
+        // If a link within the iframe is clicked
+        event.preventDefault(); // Prevent the default link behavior
+        const link = event.target.getAttribute('src'); // Get the link URL
+        console.log("graceal1 link should be ");
+        console.log(link);
+        // Open the link in a new tab or window
+        window.open("https://ideas-digitaltwin.jpl.nasa.gov/airquality/dat/", '_blank');
+      }
+    });*/
 
     // Load the initial URL
-    this.loadURL(path);
+    /*this.loadURL(path);
   }
 
   private handleIFrameLoad(event: Event) {
     console.log("graceal1 in handle iframe load with ");
     console.log(event);
     // Check if the current URL has changed
-    if (this.iframe.contentWindow && this.currentPath !== this.iframe.contentWindow.location.href) {
+    console.log("graceal1 trying to figure out why not going into if statement");
+    console.log(this.iframe.contentWindow);
+    let temp = this.iframe.contentWindow ? this.iframe.contentWindow.location: "content window is null"
+    console.log(temp);*/
+    /*if (this.iframe.contentWindow && this.currentPath !== this.iframe.contentWindow.location.href) {
+      console.log("graceal1 content window location href");
+      console.log(this.iframe.contentWindow.location.href);
       // Update the current path
       this.currentPath = this.iframe.contentWindow.location.href;
       // Load the new URL
-      this.loadURL(this.currentPath);
-    }
-  }
+      //this.loadURL(this.currentPath);
+      event.preventDefault(); // Prevent the default link behavior
+      if (event.target && event.target instanceof HTMLElement) {
+        console.log("graceal1 and event target is ");
+        console.log(event.target.getAttribute("src"));
+        //const link = event.target.getAttribute('href'); // Get the link URL
+        // Open the link in a new tab or window
+        window.open("https://ideas-digitaltwin.jpl.nasa.gov/airquality/dat/", '_blank');
+      }
+    }*/
+  //}
 
-  private loadURL(path: string) {
+  /*private loadURL(path: string) {
     // set proxy to EDSC
     console.log("graceal1 in load url ");
     console.log(path);
@@ -127,7 +236,7 @@ export class IFrameWidget extends Widget {
       }
     });
   }
-}
+}*/
 
 
 //
